63149e4b2ebb34d664d5235eadc0c083aea11354
portable <qxent300@gmail.com>
2021-04-14 18:51:59 +0900

    JPA Lombok (2)

--- src/main/java/com/fastcampus/javaallinone/project3/mycontact/domain/Person.java
+++ src/main/java/com/fastcampus/javaallinone/project3/mycontact/domain/Person.java
@@ -1,8 +1,6 @@
 package com.fastcampus.javaallinone.project3.mycontact.domain;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -10,21 +8,25 @@ import javax.persistence.Id;
 import java.time.LocalDate;
 
 @Entity
-@Getter
-@Setter
-@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@RequiredArgsConstructor    // 필요한 인자(@NonNull 로 표시)를 가지고 생성하는 생성자
+@Data                       // @Getter, @Setter, @ToString, @EqualsAndHashCode(override하여 사용할 경우 오류가 발생할 수 있으므로 제공되는 어노테이션 사용) 모두 포함
 public class Person {
 
     @Id
     @GeneratedValue
     private Long id;
 
+    @NonNull
     private String name;
 
-    private int age;
+    @NonNull
+    private Integer age;
 
     private String hobby;
 
+    @NonNull
     private String bloodType;
 
     private String address;
@@ -35,5 +37,28 @@ public class Person {
 
     @ToString.Exclude   // 데이터 숨김
     private String phoneNumber;
+
+//    public boolean equals(Object object) {
+//
+//        if (object == null) {
+//            return false;
+//        }
+//
+//        Person person = (Person) object;
+//
+//        if (!person.getName().equals(this.getName())) {
+//            return false;
+//        }
+//
+//        if (person.getAge() != this.getAge()) {
+//            return false;
+//        }
+//
+//        return true;
+//    }
+//
+//    public int hashCode() {
+//        return (name + age).hashCode();
+//    }
 
 }
--- src/test/java/com/fastcampus/javaallinone/project3/mycontact/repository/PersonRepositoryTest.java
+++ src/test/java/com/fastcampus/javaallinone/project3/mycontact/repository/PersonRepositoryTest.java
@@ -5,7 +5,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDate;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -34,6 +37,27 @@ class PersonRepositoryTest {
         assertThat(people.get(0).getAge()).isEqualTo(10);
         assertThat(people.get(0).getBloodType()).isEqualTo("A");
 
+    }
+
+    @Test
+    void hashCodeAndEquals() {
+        Person person1 = new Person("martin", 10, "A");
+        Person person2 = new Person("martin", 10, "B");
+        Person person3 = new Person("martin", 10, "A");
+
+        System.out.println("person1.equals(person2) 결과 : " + person1.equals(person2));
+        System.out.println("person1.equals(person3) 결과 : " + person1.equals(person3));
+
+        System.out.println(person1.hashCode());
+        System.out.println(person2.hashCode());
+        System.out.println(person3.hashCode());
+
+        Map<Person, Integer> map = new HashMap<>();
+        map.put(person1, person1.getAge());
+
+        System.out.println(map);
+        System.out.println(map.get(person2));
+        System.out.println(map.get(person3));
     }
 
 }
