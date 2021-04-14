58fe4f80e80e124fd18ada2cc68f0cbe7cf666b8
portable <qxent300@gmail.com>
2021-04-14 18:05:40 +0900

    JPA Lombok (1)

--- src/test/java/com/fastcampus/javaallinone/project3/mycontact/repository/PersonRepositoryTest.java
+++ src/test/java/com/fastcampus/javaallinone/project3/mycontact/repository/PersonRepositoryTest.java
@@ -8,7 +8,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class PersonRepositoryTest {
@@ -22,15 +21,18 @@ class PersonRepositoryTest {
         Person person = new Person();
         person.setName("martin");
         person.setAge(10);
+        person.setBloodType("A");
 
         personRepository.save(person);
 
-//        System.out.println(personRepository.findAll());
+        System.out.println(personRepository.findAll());
+
         List<Person> people = personRepository.findAll();
 
         assertThat(people.size()).isEqualTo(1);
         assertThat(people.get(0).getName()).isEqualTo("martin");
         assertThat(people.get(0).getAge()).isEqualTo(10);
+        assertThat(people.get(0).getBloodType()).isEqualTo("A");
 
     }
 
