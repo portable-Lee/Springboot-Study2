a7ce41558e36d17cef5a5a657fd499ac3f131fca
portable <qxent300@gmail.com>
2021-04-23 14:50:37 +0900

    [Mock-Test] Service Test #1

--- src/main/java/com/fastcampus/javaallinone/project3/mycontact/repository/PersonRepository.java
+++ src/main/java/com/fastcampus/javaallinone/project3/mycontact/repository/PersonRepository.java
@@ -5,14 +5,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.time.LocalDate;
 import java.util.List;
 
 public interface PersonRepository extends JpaRepository<Person, Long> {
 
     List<Person> findByName(String name);   // where person = ?
 
-    @Query(value = "select person from Person person where person.birthday.monthOfBirthday = :monthOfBirthday")   // ?1 = int monthOfBirthday, ?2 = int dayOfBirthday    / nativeQuery
+    @Query(value = "select person from Person person where person.birthday.monthOfBirthday = :monthOfBirthday")   // ?1 = int monthOfBirthday, ?2 = int dayOfBirthday / @Param("변수명")으로 가져온 값을 :변수명 에 대입 / nativeQuery
     List<Person> findByMonthOfBirthday(@Param("monthOfBirthday") int monthOfBirthday);
 
     @Query(value = "select * from Person person where person.deleted = true", nativeQuery = true)   // nativeQuery : 작성된 query문으로 실행
--- src/test/java/com/fastcampus/javaallinone/project3/mycontact/service/PersonServiceTest.java
+++ src/test/java/com/fastcampus/javaallinone/project3/mycontact/service/PersonServiceTest.java
@@ -2,27 +2,32 @@ package com.fastcampus.javaallinone.project3.mycontact.service;
 
 import com.fastcampus.javaallinone.project3.mycontact.domain.Person;
 import com.fastcampus.javaallinone.project3.mycontact.repository.PersonRepository;
-import org.junit.jupiter.api.MethodOrderer;
+import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)     // Mockito를 사용하는 이유 : 해당 test에 대해 더 자세히 검증 가능
 class PersonServiceTest {   // 해당 class에서 ctrl + shift + t 단축기를 누르면 Test Class 생성 가능
 
-    @Autowired
+    @InjectMocks    // 해당 test의 class(현재 PersonServiceTest 이므로 PersonService에 사용)
     private PersonService personService;
 
-    @Autowired
+    @Mock           // Autowired 하는 class
     private PersonRepository personRepository;
 
     @Test
     void getPeopleByName() {
+        when(personRepository.findByName("martin")).thenReturn(Lists.newArrayList(new Person("martin")));   // if문과 같으나 실제로 실행되는것이 아니라 실행되었다고 가정함
+                                                                                                                  // PersonRepository에서 Query문을 변경하여도 오류 없이 정상적으로 실행함
+
         List<Person> result = personService.getPeopleByName("martin");
 
         assertThat(result.size()).isEqualTo(1);
